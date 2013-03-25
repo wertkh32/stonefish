c5f61077239c4e3aaae2de1f24b900de6d0f5a82
unknown <wertkh32@wertkh32-PC.(none)>
2013-03-25 16:54:30 +0800

    some mesh updates

--- feline/Mesh.cpp
+++ feline/Mesh.cpp
@@ -6,6 +6,42 @@ Mesh::Mesh(Node node_list[], int n)
 	for(int i=0;i<n;i++)
 		nodes.push(node_list+i);
 	max_index = n;
+
+	globalStiffness = (float**)malloc(sizeof(float*) * n * 3);
+	globalMass = (float**)malloc(sizeof(float*) * n * 3);
+	for(int i=0;i<n;i++)
+	{
+		globalStiffness[i] = (float*)malloc(sizeof(float) * n * 3);
+		globalMass[i] = (float*)malloc(sizeof(float) * n * 3);
+	}
+
+	resetGlobalStiffness();
+	resetGlobalMass();
+}
+
+void
+Mesh::assembleMassMat()
+{
+	for(int i=0;i<max_index;i++)
+		for(int j=0;j<max_index;j++)
+		{
+		}
+}
+
+void
+Mesh::resetGlobalStiffness()
+{
+	for(int i=0;i<max_index;i++)
+		for(int j=0;j<max_index;j++)
+			globalStiffness[i][j] = 0.0;
+}
+
+void
+Mesh::resetGlobalMass()
+{
+	for(int i=0;i<max_index;i++)
+		for(int j=0;j<max_index;j++)
+			globalMass[i][j] = 0.0;
 }
 
 void
--- feline/Mesh.h
+++ feline/Mesh.h
@@ -9,12 +9,16 @@ class Mesh
 public:
 	QuickArray<Element*,MAX_ELEMENTS> elements;
 	QuickArray<Node*,MAX_NODES> nodes;
+	float **globalStiffness, **globalMass;
+
 
 	int getNoOfElements(){return elements.size();}
 	int getNoOfNodes(){return max_index;}
 	
 	void addElement(int ind0, int ind1, int ind2, int ind3, float _E, float _v, float density);
-
+	void resetGlobalStiffness();
+	void resetGlobalMass();
+	void assembleMassMat();
 
 	Mesh(Node nodes[], int n);
 	~Mesh(void);
