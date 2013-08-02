33b546d974df07ab201e808c1fa829ffde654757
unknown <wertkh32@wertkh32-PC.(none)>
2013-08-03 05:21:05 +0800

    additional stuff

---
+++ feline/ConstrainedRows.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "QuickArray.h"
+#define MAX_ROWS_CONSTRAINED 50
+
+enum DOF
+{
+	X = 0, Y = 1, Z = 2
+};
+
+struct ConstrainedRows
+{
+	QuickArray<int,MAX_ROWS_CONSTRAINED> list;
+	ConstrainedRows()
+	{
+	}
+	
+	void add(int node, DOF dof)
+	{
+		list.push(node * 3 + dof);
+	}
+	
+	void add(int node)
+	{
+		list.push(node * 3);
+		list.push(node * 3 + 1);
+		list.push(node * 3 + 2);
+	}
+
+
+};
---
+++ feline/GPUIntegratorFuncs.cuh
@@ -0,0 +1,15 @@
+#pragma once
+#include "GPUDataStructs.cuh"
+
+void gpuUploadVars(GPUElement* gpuElements, GPUNode* gpuNodes,float* xt, 
+			  float* vt, float* extforces, int numnodes, int numelements);
+
+void gpuUploadExtForces(float* extforces, int numnodes);
+
+void gpuDownloadVars(float* xt, int numnodes);
+
+void gpuInitVars(int numele, int numnodes);
+
+void gpuTimeStep(int numelements, int numnodes);
+
+void gpuDestroyVars();
